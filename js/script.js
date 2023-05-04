addbedc0fc718e269a456393e451470901014b56
hyperstone1 <kingofthegorgon@mail.ru>
2023-05-04 22:00:04 +0300

    test_nti

--- js/script.js
+++ js/script.js
@@ -1,21 +1,36 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const body = document.querySelector('body');
+  const header = document.querySelector('header');
+  //элементы, которые необходимо изменять при разрешении экрана для моб.
   const notebookBlur = document.querySelector('.best_products_right__notebook img');
   const cardTitle = document.querySelector('.card_title');
   const cardSubTitle4 = document.querySelectorAll('.card_description')[3];
   const cardTitle4 = document.querySelectorAll('.card_title')[3];
   const contactsContainer = document.querySelector('.contacts_container');
-  const body = document.querySelector('body');
+
+  //Кнопка бургер-меню
   const menuBtn = document.querySelector('.menu-btn');
+
+  // элементы бергер меню
   const menu = document.querySelector('.burger-menu');
   const popup = document.querySelector('.popup');
+
+  //иконка юзера и меню для него
   const user = document.querySelector('.user');
   const userNav = document.querySelector('.user_nav');
-  const header = document.querySelector('header');
 
+  // Форма и инпуты в ней
+  const form = document.querySelector('form');
+  const nameInput = document.getElementById('fullname');
+  const phoneInput = document.getElementById('phone');
+  const commentInput = document.getElementById('comment');
+
+  // Функция получения разреров экрана
   function handleWindowResize() {
     const { width: windowWidth, height: windowHeight } = screen;
     console.log(`Ширина окна: ${windowWidth} Высота окна: ${windowHeight}`);
 
+    // Изменение контента внутри элементов
     if (windowWidth <= 375) {
       notebookBlur.src = './assets/notebook_blur_2.png';
       cardTitle.textContent = 'У нас самые выгодные и низкие цены';
@@ -27,9 +42,14 @@ document.addEventListener('DOMContentLoaded', () => {
       if (!contactsContainer.classList.contains('container')) {
         contactsContainer.classList.add('container');
       }
+      notebookBlur.src = './assets/notebook_blur.png';
+      cardTitle.textContent = 'Самые выгодные и низкие цены';
+      cardSubTitle4.textContent = 'Официальный поставщик продукции DELL в России и странах СНГ';
+      cardTitle4.textContent = 'Официальный партнер DELL';
     }
   }
 
+  // Обработчик беругер-меню
   function closeMenu(event) {
     const menuBtnClicked = event.target.closest('.menu-btn');
     const burgerMenuClicked = event.target.closest('.burger-menu');
@@ -42,7 +62,53 @@ document.addEventListener('DOMContentLoaded', () => {
       document.removeEventListener('click', closeMenu);
     }
   }
+  // Отправка формы
+  form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    console.log(nameInput.value);
+    const errors = {};
+    // проверяем корректность имени
+    if (!/^[a-zA-Zа-яА-Я ]{2,30}$/.test(nameInput.value)) {
+      nameInput.classList.add('error');
+      errors.name = 'Пожалуйста, введите корректное имя';
+    } else {
+      if (nameInput.classList.contains('error')) {
+        nameInput.classList.remove('error');
+      }
+      delete errors.name;
+    }
 
+    // проверяем корректность номера телефона
+    if (!/^\+?[0-9]{8,14}$/.test(phoneInput.value)) {
+      phoneInput.classList.add('error');
+      errors.phone = 'Пожалуйста, введите корректный номер телефона';
+    } else {
+      if (phoneInput.classList.contains('error')) {
+        phoneInput.classList.remove('error');
+      }
+      delete errors.phoneInput;
+    }
+
+    // проверяем корректность комментария
+    if (commentInput.value.length < 10 || commentInput.value.length > 500) {
+      errors.comments = 'Пожалуйста, введите комментарий длиной от 10 до 500 символов';
+      commentInput.classList.add('error');
+    } else {
+      if (commentInput.classList.contains('error')) {
+        commentInput.classList.remove('error');
+      }
+      delete errors.comments;
+    }
+
+    if (Object.keys(errors).length > 0) {
+      alert('Пожалуйста, введите корректные данные');
+    } else {
+      // если все проверки пройдены успешно, отправляем форму
+      form.submit();
+    }
+  });
+
+  // Слайдер для best_products
   const swiper = new Swiper('.swiper-container', {
     slidesPerView: 1,
     spaceBetween: 30,
@@ -53,14 +119,15 @@ document.addEventListener('DOMContentLoaded', () => {
     },
   });
 
+  // Ивенты для юзер меню
   header.addEventListener('mouseleave', function () {
     userNav.classList.remove('active');
   });
-
   user.addEventListener('mouseenter', function () {
     userNav.classList.add('active');
   });
 
+  // ивент для открытия/закрытия меню
   menuBtn.addEventListener('click', function toggleMenu() {
     menu.classList.toggle('active');
     popup.classList.toggle('active');
@@ -71,7 +138,7 @@ document.addEventListener('DOMContentLoaded', () => {
       document.removeEventListener('click', closeMenu);
     }
   });
-
+  // слушатели для получения разреров экрана при загрузке DOM и при изменении разреров экрана
   window.addEventListener('DOMContentLoaded', handleWindowResize);
   window.addEventListener('resize', handleWindowResize);
 });
